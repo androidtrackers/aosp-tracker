3f6968e3b11f8f7f6a11fa19136396a9ee1e4361
yshalsager <ysh-alsager@hotmail.com>
2019-08-05 17:26:40 +0200

    refactor - add security patch tracking

--- aosp_tracker.py
+++ aosp_tracker.py
@@ -1,74 +1,141 @@
+#!/usr/bin/env python3.7
+"""AOSP tracker"""
+
 import difflib
 from datetime import date
-from bs4 import BeautifulSoup
 from os import environ, rename, path, system
+from bs4 import BeautifulSoup
 from requests import get, post
 
-today = str(date.today())
-telegram_chat = "@aosptracker"
-bottoken = environ['bottoken']
+URL = 'https://android.googlesource.com/platform/frameworks/base/+refs'
+
+TG_CHAT = "@aosptracker"
+BOT_TOKEN = environ['bottoken']
 GIT_OAUTH_TOKEN = environ['XFU']
+BRANCHES = []
+TAGS = []
 
-url = 'https://android.googlesource.com/platform/frameworks/base/+refs'
-response = get(url)
-page = BeautifulSoup(response.content, 'html.parser')
-data = page.findAll("div", {"class": "RefList"})
-branches = []
-tags = []
-for section in data:
-    title = section.find("h3", {"class": "RefList-title"})
-    values = section.find_all("li")
-    if "Branches" in title.text:
-        for value in values:
-            branches.append(value.text)
-    elif "Tags" in title.text:
-        for value in values:
-            tags.append(value.text)
 
-types = {'branches': branches, 'tags': tags}
-for key, value in types.items():
-    if path.exists(str(key)):
-        rename(str(key), str(key) + '_old')
-    # save to file
-    with open(str(key), 'w') as o:
-        for i in value:
-            o.write(i + '\n')
+def fetch():
+    """
+    fetch latest info
+    """
+    response = get(URL)
+    page = BeautifulSoup(response.content, 'html.parser')
+    data = page.findAll("div", {"class": "RefList"})
+    for section in data:
+        title = section.find("h3", {"class": "RefList-title"})
+        values = section.find_all("li")
+        if "Branches" in title.text:
+            for value in values:
+                BRANCHES.append(value.text)
+        elif "Tags" in title.text:
+            for value in values:
+                TAGS.append(value.text)
+
+
+def diff_files():
+    """
+    diff
+    """
     # diff
-    with open(str(key) + '_old', 'r') as old, open(str(key), 'r') as new:
-        o = old.readlines()
-        n = new.readlines()
-    diff = difflib.unified_diff(o, n, fromfile=str(key) + '_old', tofile=str(key))
-    changes = []
-    for line in diff:
-        if line.startswith('+'):
-            changes.append(str(line))
-    new = ''.join(changes[1:]).replace("+", "")
-    with open(str(key) + '_changes', 'w') as o:
-        o.write(new)
-    # post to tg
-    with open(str(key) + '_changes', 'r') as c:
-        for line in c:
-            if key is 'branches':
-                t = 'branch'
-            elif key is 'tags':
-                t = 'tag'
-            telegram_message = "New {0} detected!: `{1}`" \
-                               "[Check Here]({2})".format(t, line, url.split('/+')[0] + '/+/' + line)
-            params = (
-                ('chat_id', telegram_chat),
-                ('text', telegram_message),
-                ('parse_mode', "Markdown"),
-                ('disable_web_page_preview', "yes")
-            )
-            telegram_url = "https://api.telegram.org/bot" + bottoken + "/sendMessage"
-            telegram_req = post(telegram_url, params=params)
-            telegram_status = telegram_req.status_code
-            if telegram_status == 200:
-                print("{0}: Telegram Message sent".format(line))
-            else:
-                print("Telegram Error")
-# commit and push
-system("git add branches tags && git -c \"user.name=XiaomiFirmwareUpdater\" "
-       "-c \"user.email=xiaomifirmwareupdater@gmail.com\" commit -m \"[skip ci] sync: {0}\" && "" \
-       ""git push -q https://{1}@github.com/androidtrackers/aosp-tracker.git HEAD:master"
-       .format(today, GIT_OAUTH_TOKEN))
+    types = {'branches': BRANCHES, 'tags': TAGS}
+    for key, value in types.items():
+        if path.exists(str(key)):
+            rename(str(key), str(key) + '_old')
+        # save to file
+        with open(str(key), 'w') as out:
+            for i in value:
+                out.write(i + '\n')
+        with open(str(key) + '_old', 'r') as old, open(str(key), 'r') as new:
+            diff = difflib.unified_diff(old.readlines(), new.readlines(),
+                                        fromfile=str(key) + '_old', tofile=str(key))
+        changes = []
+        for line in diff:
+            if line.startswith('+'):
+                changes.append(str(line))
+        new = ''.join(changes[1:]).replace("+", "")
+        with open(str(key) + '_changes', 'w') as out:
+            out.write(new)
+        # post to tg
+        with open(str(key) + '_changes', 'r') as changes:
+            for line in changes:
+                if key == 'branches':
+                    type_ = 'branch'
+                elif key == 'tags':
+                    type_ = 'tag'
+                telegram_message = "New {0} detected! `{1}`" \
+                                   "[Check Here]({2})"\
+                    .format(type_, line, URL.split('/+')[0] + '/+/' + line)
+                post_to_tg(telegram_message)
+
+
+def post_to_tg(telegram_message):
+    """
+    post new devices to telegram channel
+    """
+    params = (
+        ('chat_id', TG_CHAT),
+        ('text', telegram_message),
+        ('parse_mode', "Markdown"),
+        ('disable_web_page_preview', "yes")
+    )
+    telegram_url = "https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage"
+    telegram_req = post(telegram_url, params=params)
+    telegram_status = telegram_req.status_code
+    if telegram_status == 200:
+        print("Telegram Message sent")
+    else:
+        print("Telegram Error")
+
+
+def git_commit_push():
+    """
+    git add - git commit - git push
+=   """
+    today = str(date.today())
+    system("git add branches tags security_patch && git -c \"user.name=XiaomiFirmwareUpdater\" "
+           "-c \"user.email=xiaomifirmwareupdater@gmail.com\""
+           " commit -m \"[skip ci] sync: {0}\" && "" \
+           ""git push -q https://{1}@github.com/androidtrackers/aosp-tracker.git HEAD:master"
+           .format(today, GIT_OAUTH_TOKEN))
+
+
+def security_bulletin():
+    """
+    Android Security Bulletins
+    """
+    data = BeautifulSoup(
+        get('https://source.android.com/security/bulletin').content, 'html.parser')\
+        .find('table').findAll('tr')[1]
+    latest = data.findAll('td')[0].text
+    link = 'https://source.android.com/' + data.findAll('td')[0].a['href']
+    patch = data.findAll('td')[-1].text.replace(' ', '')
+    if path.exists('security_patch'):
+        rename('security_patch', 'security_patch_old')
+    with open('security_patch', 'w') as out:
+        out.write(latest)
+    with open('security_patch_old', 'r') as old_file:
+        old = old_file.read()
+    if latest != old:
+        message = f'New Security Patch detected!\n' \
+            f'[{latest}]({link})\n' \
+            f'__Patch__:\n' \
+            f'{patch}'
+        post_to_tg(message)
+    else:
+        return
+
+
+def main():
+    """
+    Main scraping script
+    """
+    fetch()
+    diff_files()
+    security_bulletin()
+    git_commit_push()
+
+
+if __name__ == '__main__':
+    main()
