0751901111cd250bf4e26efb675731d69dcfbe4a
yshalsager <ysh-alsager@hotmail.com>
2022-08-08 15:53:00 +0200

    sbl: Add patch commits support
    Signed-off-by: yshalsager <ysh-alsager@hotmail.com>

--- aosp_tracker.py
+++ aosp_tracker.py
@@ -3,15 +3,17 @@
 
 import difflib
 from datetime import date
+from itertools import groupby
 from os import environ, rename, path, system
+
 from bs4 import BeautifulSoup
 from requests import get, post
 
-URL = 'https://android.googlesource.com/platform/frameworks/base/+refs'
+URL = "https://android.googlesource.com/platform/frameworks/base/+refs"
 
 TG_CHAT = "@aosptracker"
-BOT_TOKEN = environ['bottoken']
-GIT_OAUTH_TOKEN = environ['XFU']
+BOT_TOKEN = environ["bottoken"]
+GIT_OAUTH_TOKEN = environ["XFU"]
 BRANCHES = []
 TAGS = []
 
@@ -21,7 +23,7 @@ def fetch():
     fetch latest info
     """
     response = get(URL)
-    page = BeautifulSoup(response.content, 'html.parser')
+    page = BeautifulSoup(response.content, "html.parser")
     data = page.findAll("div", {"class": "RefList"})
     for section in data:
         title = section.find("h3", {"class": "RefList-title"})
@@ -39,34 +41,38 @@ def diff_files():
     diff
     """
     # diff
-    types = {'branches': BRANCHES, 'tags': TAGS}
+    types = {"branches": BRANCHES, "tags": TAGS}
     for key, value in types.items():
         if path.exists(str(key)):
-            rename(str(key), str(key) + '_old')
+            rename(str(key), str(key) + "_old")
         # save to file
-        with open(str(key), 'w') as out:
+        with open(str(key), "w") as out:
             for i in value:
-                out.write(i + '\n')
-        with open(str(key) + '_old', 'r') as old, open(str(key), 'r') as new:
-            diff = difflib.unified_diff(old.readlines(), new.readlines(),
-                                        fromfile=str(key) + '_old', tofile=str(key))
+                out.write(i + "\n")
+        with open(str(key) + "_old", "r") as old, open(str(key), "r") as new:
+            diff = difflib.unified_diff(
+                old.readlines(),
+                new.readlines(),
+                fromfile=str(key) + "_old",
+                tofile=str(key),
+            )
         changes = []
         for line in diff:
-            if line.startswith('+'):
+            if line.startswith("+"):
                 changes.append(str(line))
-        new = ''.join(changes[1:]).replace("+", "")
-        with open(str(key) + '_changes', 'w') as out:
+        new = "".join(changes[1:]).replace("+", "")
+        with open(str(key) + "_changes", "w") as out:
             out.write(new)
         # post to tg
-        with open(str(key) + '_changes', 'r') as changes:
+        with open(str(key) + "_changes", "r") as changes:
             for line in changes:
-                if key == 'branches':
-                    type_ = 'branch'
-                elif key == 'tags':
-                    type_ = 'tag'
-                telegram_message = "New {0} detected! `{1}`" \
-                                   "[Check Here]({2})"\
-                    .format(type_, line, URL.split('/+')[0] + '/+/' + line)
+                if key == "branches":
+                    type_ = "branch"
+                elif key == "tags":
+                    type_ = "tag"
+                telegram_message = "New {0} detected! `{1}`" "[Check Here]({2})".format(
+                    type_, line, URL.split("/+")[0] + "/+/" + line
+                )
                 post_to_tg(telegram_message)
 
 
@@ -75,10 +81,10 @@ def post_to_tg(telegram_message):
     post new devices to telegram channel
     """
     params = (
-        ('chat_id', TG_CHAT),
-        ('text', telegram_message),
-        ('parse_mode', "Markdown"),
-        ('disable_web_page_preview', "yes")
+        ("chat_id", TG_CHAT),
+        ("text", telegram_message),
+        ("parse_mode", "Markdown"),
+        ("disable_web_page_preview", "yes"),
     )
     telegram_url = "https://api.telegram.org/bot" + BOT_TOKEN + "/sendMessage"
     telegram_req = post(telegram_url, params=params)
@@ -91,37 +97,71 @@ def post_to_tg(telegram_message):
 
 def git_commit_push():
     """
-    git add - git commit - git push
-=   """
+        git add - git commit - git push
+    ="""
     today = str(date.today())
-    system("git add branches tags security_patch && git -c \"user.name=XiaomiFirmwareUpdater\" "
-           "-c \"user.email=xiaomifirmwareupdater@gmail.com\""
-           " commit -m \"[skip ci] sync: {0}\" && "" \
-           ""git push -q https://{1}@github.com/androidtrackers/aosp-tracker.git HEAD:master"
-           .format(today, GIT_OAUTH_TOKEN))
+    system(
+        'git add branches tags security_patch && git -c "user.name=XiaomiFirmwareUpdater" '
+        '-c "user.email=xiaomifirmwareupdater@gmail.com"'
+        ' commit -m "[skip ci] sync: {0}" && '
+        " \
+           "
+        "git push -q https://{1}@github.com/androidtrackers/aosp-tracker.git HEAD:master".format(
+            today, GIT_OAUTH_TOKEN
+        )
+    )
 
 
 def security_bulletin():
     """
     Android Security Bulletins
     """
-    data = BeautifulSoup(
-        get('https://source.android.com/security/bulletin').content, 'html.parser')\
-        .find('table').findAll('tr')[1]
-    latest = data.findAll('td')[0].text
-    link = 'https://source.android.com/' + data.findAll('td')[0].a['href']
-    patch = data.findAll('td')[-1].text.replace(' ', '')
-    if path.exists('security_patch'):
-        rename('security_patch', 'security_patch_old')
-    with open('security_patch', 'w') as out:
+    data = (
+        BeautifulSoup(
+            get("https://source.android.com/security/bulletin").content, "html.parser"
+        )
+        .find("table")
+        .findAll("tr")[1]
+    )
+    latest = data.findAll("td")[0].text
+    link = "https://source.android.com/" + data.findAll("td")[0].a["href"]
+    patch = data.findAll("td")[-1].text.replace(" ", "").replace("\n", " | ")
+    sbl_patches = ""
+    sbl_response = get(f"{link}?partial=1")
+    if sbl_response.ok and sbl_response.text.startswith("["):
+        sbl_data = [
+            i for i in sbl_response.json() if i and isinstance(i, str) and "CVE" in i
+        ]
+        page = BeautifulSoup(sbl_data[0].strip(), "html.parser")
+        patches = page.select('td a[href*="android.googlesource.com"]')
+        patches_groups = [
+            list(item)
+            for _, item in groupby(
+                sorted(patches, key=lambda x: x["href"]),
+                lambda x: "_".join(x.get("href", "").split("+")[0].split("/")[3:-1]),
+            )
+        ]
+        for patches_group in patches_groups:
+            sbl_patches += f'\n`{"_".join(patches_group[0].get("href", "").split("+")[0].split("/")[3:-1])}`:\n'
+            sbl_patches += " - ".join(
+                f"[{patch.text.strip()}]({patch.get('href')})"
+                for patch in patches_group
+            )
+
+    if path.exists("security_patch"):
+        rename("security_patch", "security_patch_old")
+    with open("security_patch", "w") as out:
         out.write(latest)
-    with open('security_patch_old', 'r') as old_file:
+    with open("security_patch_old", "r") as old_file:
         old = old_file.read()
     if latest != old:
-        message = f'New Security Patch detected!\n' \
-            f'[{latest}]({link})\n' \
-            f'__Patch__:\n' \
-            f'{patch}'
+        message = (
+            f"New Security Patch detected! [{latest}]({link})\n"
+            f"__Patch__: {patch}\n\n"
+        )
+        if sbl_patches:
+            message += "**Security Patch Commits:**"
+            message += sbl_patches
         post_to_tg(message)
     else:
         return
@@ -137,5 +177,5 @@ def main():
     git_commit_push()
 
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     main()
